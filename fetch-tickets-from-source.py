5a1e50bc89ddd8c5da16b8797ae51818a4bfed19
Andrés Laurito Giber <andy.laurito@gmail.com>
2023-06-25 14:21:48 +0200

    Adding script for fetching images from drive. Text to image

---
+++ fetch-tickets-from-source.py
@@ -0,0 +1,53 @@
+import os
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload
+
+# Provide the path to your service account key JSON file
+SERVICE_ACCOUNT_FILE = 'bills-390910-64d970a32421.json'
+
+# Scopes for accessing Google Drive
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+# ID of the folder to fetch files from
+FOLDER_ID = 'ticket_compras'
+
+# Destination folder to save downloaded files
+DESTINATION_FOLDER = 'tickets'
+
+def fetch_files_from_drive():
+    # Authenticate and create a Drive API service client
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    drive_service = build('drive', 'v3', credentials=credentials)
+
+    # Fetch files from the specified folder
+    results = drive_service.files().list().execute()
+
+    files = results.get('files', [])
+    ticket_images = [aticket_image for aticket_image in results.get('files', []) if aticket_image['mimeType'] == 'image/jpeg']
+
+    if not ticket_images:
+        print('No tickets found.')
+    else:
+        print('Downloading tickets...')
+        for ticket in ticket_images:
+            file_id = ticket['id']
+            file_name = ticket['name']
+            file_path = os.path.join(DESTINATION_FOLDER, file_name)
+
+            request = drive_service.files().get_media(fileId=file_id)
+            fh = open(file_path, 'wb')
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+
+            while not done:
+                status, done = downloader.next_chunk()
+                print(f"Downloading {file_name} - {int(status.progress() * 100)}%")
+
+            print(f"{file_name} downloaded successfully.")
+
+        print('Download completed.')
+
+fetch_files_from_drive()
---
+++ get-information-from-ticket.py
@@ -0,0 +1,31 @@
+import cv2
+import pytesseract
+
+def extract_text_from_image(image_path):
+    # Read the image using OpenCV
+    image = cv2.imread(image_path)
+
+    # Preprocess the image (optional)
+    # You can apply various image preprocessing techniques here
+    # For example, resizing, denoising, thresholding, etc.
+
+    # Perform OCR using Tesseract
+    text = pytesseract.image_to_string(image)
+
+    # Split the text into lines
+    lines = text.split('\n')
+
+    # Remove empty lines
+    lines = [line.strip() for line in lines if line.strip()]
+
+    return lines
+
+# Provide the path to your image containing the ticket
+image_path = 'tickets/ticket1.jpg'
+
+# Extract text from the image
+result = extract_text_from_image(image_path)
+
+# Print the extracted lines
+for line in result:
+    print(line)
